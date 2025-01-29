a395690482594ae3c2b06f92692c91184bd92e73
Jaco777 <jaco777thomas@gmail.com>
2025-01-29 20:10:21 +0530

    initial commit with test code

---
+++ GeminiApiHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+public class GeminiApiHelper
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    public static async Task<string> CallGeminiApi(string apiKey, string endpoint, string requestData)
+    {
+        client.DefaultRequestHeaders.Clear();
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+        var content = new StringContent(requestData, Encoding.UTF8, "application/json");
+
+        HttpResponseMessage response = await client.PostAsync(endpoint, content);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception($"Error calling API: {response.ReasonPhrase}");
+        }
+
+        return await response.Content.ReadAsStringAsync();
+    }
+}
